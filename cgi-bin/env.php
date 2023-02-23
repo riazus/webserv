07bd1d48d4987fd5b0e0ca931faccbf4de86e943
Riyaz Usmanov <usmanov.riyaz22@gmail.com>
2023-02-23 19:55:06 +0100

    dev

--- cgi-bin/env.php
+++ cgi-bin/env.php
@@ -1,15 +1,25 @@
 <?php
 
 
+echo '<html>';
+
+# echo '<head>'
+# echo '</head>'
+echo '<body bgcolor="#c0c0c0"';
+echo '<center>';
 
 //Call getenv() function without argument
 $env_array =getenv();
-echo "<h3>The list of environment variables with values are :</h3>";
+// echo "<h3>The list of environment variables with values are :</h3>";
 
 //Print all environment variable names with values
 foreach ($env_array as $key=>$value)
 {
     echo "$key => $value \r\n\n";
 }
+echo '</center>';
+
+echo '</body>';
+echo '</html>';
 
 ?>
